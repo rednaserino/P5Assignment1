cd9d249a46aa5e84f29582cb7b2ede32cece9596
Sander Tant <Sander.Tant@telenet.be>
2020-03-05 21:35:09 +0100

    added ranOutOfTime

--- assignmentOne/src/app/game/services/counter.service.ts
+++ assignmentOne/src/app/game/services/counter.service.ts
@@ -1,25 +1,25 @@
-import { Injectable } from '@angular/core';
-import { observable, BehaviorSubject, timer, NEVER } from 'rxjs';
-import { switchMap, map, takeWhile } from 'rxjs/operators'; 
+import { Injectable } from "@angular/core";
+import { observable, BehaviorSubject, timer, NEVER } from "rxjs";
+import { switchMap, map, takeWhile } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class CounterService {
-  initialValue = 100;
+  initialValue = 10;
   toggleCounter$ = new BehaviorSubject(false);
   toRemainder = (t: number) => this.initialValue - t;
 
   remainingSeconds$ = this.toggleCounter$.pipe(
     switchMap((running: boolean) => (running ? timer(0, 1000) : NEVER)),
     map(this.toRemainder),
-    takeWhile(t => t >= 0),
+    takeWhile(t => t >= 0)
   );
 
-  start(){
+  start() {
     this.toggleCounter$.next(true);
   }
-  stop(){
+  stop() {
     this.toggleCounter$.next(false);
   }
 }
